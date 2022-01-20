0a0c1e8285f2dae56c504c476fcfd132f401c394
Rahil Rahimi <rahilrahimi1362@gmail.com>
2022-01-19 21:33:25 -0500

    initial change

--- server/server.js
+++ server/server.js
@@ -5,12 +5,15 @@ const { ApolloServer } = require('apollo-server-express');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({ 
   typeDefs, 
-  resolvers,   
+  resolvers, 
+  context: authMiddleware
 });
 server.applyMiddleware({ app });
 
