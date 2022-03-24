71cd889e7d62d9e54354bf8b8ef646c7ea766cbf
Rahil Rahimi <rahilrahimi1362@gmail.com>
2022-03-24 15:01:20 -0400

    iniial change

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,7 @@ import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
 import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 
 const Home = () => {
@@ -19,6 +20,11 @@ const { data: userData } = useQuery(QUERY_ME_BASIC);
   return (
 <main>
   <div className="flex-row justify-space-between">
+    {loggedIn && (
+     <div className="col-12 mb-3">
+      <ThoughtForm />
+      </div>
+    )}
     <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
       {loading ? (
         <div>Loading...</div>
